255581818c99cbaa348e35c3248d94f327f08a19
Alexis Torres <alpixels@Europa>
2021-08-02 14:01:19 -0500

    Creación del código para determinar el número de coordinación a partir de la integral de la función de distribución radial desde cero hasta su primer minimo

--- README.md
+++ README.md
@@ -11,13 +11,16 @@ file is especified by means of the volume fraction within the code
 
 
 Code: PressGrv2.f90
+
 Description: Computes the pressure for a square-well fluid using the radial
 distribution function and the approaches of Henderson and Tago. This procedure 
 requeries the contact value of g(r), and the values at the potential
 discontinuities, this values are computed with an extrapolation 
 of the g(r) present data employing a third degree Lagrange polynomial function
 
+
 Code: PressTW.f90
+
 Description: Computes the pressure for a triangular well fluid using the radial
 distribution function separating the potential contributions on the hard-sphere 
 and triangular contribs, the first one is computed with the Carnahan-Starling 
@@ -26,3 +29,11 @@ derivative times the radial distribution function. This procedure requeries the
 contact value of g(r), which is computed using a third degree Lagrange polynom 
 to extrapolate the value. Hence, provide the file with the g(r) data as well as
 interaction range (lambda), density and temperatures is mandatory.
+
+
+Code: nCoordination
+
+Description: Determines the first coordination number in three dimensions using 
+the radial distribution function. Program requieres temperature, number density 
+and particle number at which the g(r) was determined, however, temperature it is 
+not used at any computation stage, is used only to give context to results
---
+++ nCoordination.f90
@@ -0,0 +1,98 @@
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!%%**************************************************************************%%
+!%%** PROGRAM     <n> COORDINATION NUMBER FROM g(r)                        **%%
+!%%**                                                                      **%%
+!%%** AUTHOR      ALEXIS TORRES CARBAJAL                                   **%%
+!%%** DATE        AGUST 2, 2021                                            **%%
+!%%** VERSION     V00                                                      **%%
+!%%** LICENSE     LGPL-V3                                                  **%%
+!%%**                                                                      **%%
+!%%** OBS         THE COMPUTATION DEPENDS ON THE FILE OF g(r)              **%%
+!%%**             PROGRAM DEMANDS TEMPERATURE (TSTAR), NUMBER DENSITY (RHO)**%%
+!%%**             AND PARTICLE NUMBER (NPART) AT WHICH g(r) WAS DETERMINED **%%
+!%%**             SINCE FOR r < SIGMA g(r) IS EQUAL TO ZERO THE FIRST      **%%
+!%%**             MINIMUM IS LOCATED AFTER THE FIRST MAXIMUM, THUS, THIS   **%%
+!%%**             DETERMINATED FIRST AND TAKEN AS REFERENCE                **%%
+!%%**************************************************************************%%
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+MODULE CORVAR
+ INTEGER, PARAMETER:: D     = KIND(1.0D0)   !PRECISION
+ INTEGER, PARAMETER:: NDATX = 5000          !MAXIMUM NUMBER OF DATA
+
+ REAL(D), PARAMETER:: ESTAR = 1.0D0         !ATTRACTIVE STRENGHT
+ REAL(D), PARAMETER:: PI    = DACOS(-1.0D0) !PI NUMBER
+ REAL(D), PARAMETER:: DBIN  = 0.01D0        !BIN WIDTH
+ REAL(D), PARAMETER:: SIGMA = 1.0D0         !MOLECULES DIAMETER
+
+ INTEGER:: I,NPART,NBIN,NST
+ REAL(D):: RI(NDATX),GRI(NDATX)
+ REAL(D):: RHO,TSTAR,BOXX,NCOR
+ REAL(D):: GMIN,GMAX
+
+ REAL(D):: FX,INTE,XI0,XI1,XI2
+END MODULE CORVAR
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%   MAIN PROGRAM   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+PROGRAM NCOORDINATION
+ USE CORVAR
+
+ OPEN(UNIT=10,FILE='MDGr.dat',STATUS='OLD')
+ REWIND(10) 
+
+ NPART=1372                                  !NUMBER OF PARTICLES
+ TSTAR=0.450D0                               !SYSTEM TEMPERATURE
+ RHO=0.76394373                              !SYSTEM DENSITY
+ BOXX=(DBLE(NPART)/RHO)**(1.0D0/3.0D0)       !SIMULATION BOX LENGHT
+ NBIN=INT(0.5D0*BOXX/DBIN) - 1               !NUMBER OF DATA IN g(r) FILE
+
+! READ DATA FROM FILE
+ DO I=1,NBIN
+    READ(10,*)RI(I),GRI(I)
+ ENDDO
+ 
+!THE FIRST MAXIMUM IS IDENTIFIED
+ GMAX=0.0D0
+ DO I=1,NBIN
+    GMAX=MAX(GMAX,GRI(I))
+    IF(GMAX .GT. GRI(I))THEN
+      NST=I
+      EXIT
+    ENDIF
+ ENDDO
+
+!NOW THE FIRS MIMIMUN IS IDENTIFIED TO SET INTEGRAL RANGE
+ GMIN=100.0D0
+ DO I=NST,NBIN
+    GMIN=MIN(GMIN,GRI(I))
+    IF((GMIN .LT. GRI(I-1)) .AND. (GMIN .LT. GRI(I+1)))THEN
+       NDAT=I
+       EXIT
+    ENDIF
+ ENDDO
+
+!INTEGRATION SCHEME WITH THE COMPOSSED SIMPSON RULE
+ FX=GRI(1)*RI(1)*RI(1)
+ XI0=FX
+ FX=GRI(NDAT)*RI(NDAT)*RI(NDAT)
+ XI0=XI0 + FX
+
+ XI1=0.0D0
+ XI2=0.0D0
+
+ DO I=2,NDAT-1
+    FX=GRI(I)*RI(I)*RI(I)
+    IF(MOD(I,2) .EQ. 0)THEN
+      XI2=XI2 + FX  
+    ELSE
+      XI1=XI1 + FX  
+    ENDIF
+ ENDDO
+
+!COORDINATION NUMBER INTEGRAL
+ INTE=DBIN*( XI0 + 2.0D0*XI2 + 4.0D0*XI1)/3.0D0
+ NCOR=4.0D0*PI*RHO*INTE
+
+ WRITE(6,*)TSTAR,RHO,NCOR
+ STOP
+END PROGRAM NCOORDINATION
